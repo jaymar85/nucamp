0bf45ae11ad758a825a4464d58f8255c8fa52a52
jaymar85 <jermark851@gmail.com>
2020-08-06 22:17:39 -0500

    jquery file uploader

--- 1-HTML-CSS-JavaScript/workshop-4/array-methods.js
+++ 1-HTML-CSS-JavaScript/workshop-4/array-methods.js
@@ -1,25 +1,23 @@
 let galsArray = ["Cece", "Melissa", "Stacy", "Emily"]
-let guysArray = ["Micheal", "Seth", "Jose", "Aimiable"]
-
-// Review of last week.........
-
-// console.log();
+let guysArray = ["Micheal", "Seth", "Jose", "Aimable"]
 
 // Take a wild educated guess?
 
 ////Concat:
-let classArray = galsArray.concat(guysArray);
+// let classArray = galsArray.concat(guysArray);
 // console.log(classArray);
 
 //// Sort:
 // console.log(classArray.sort());
 
 //// Reverse:
-// console.log(classArray.reverse())
+// console.log(classArray.reverse());
 
 //// Slice:
 // console.log(galsArray.slice(1, 3));
 
 //// Splice:
-guysArray.splice(2, 1, "Sam", "Nicholas");
-// console.log(guysArray);
+let newGuysArr = guysArray.splice(2, 1);
+console.log(newGuysArr);
+console.log(guysArray);
+
---
+++ 1-HTML-CSS-JavaScript/workshop-4/loops.js
@@ -0,0 +1,18 @@
+// For Loops
+
+let colorsArray = ['red', 'blue', 'green', 'cyan']
+
+// for (let i = 0; i < colorsArray.length; i++) {
+//     console.log(colorsArray[i]);
+// }
+
+// for (let color of colorsArray) {
+//     console.log(color);
+// }
+
+// let x = 5;  
+
+// for (let i = 1; i <= 5; i++) {                                           
+//      x = x * i;              
+//      console.log(x);                                   
+// }
