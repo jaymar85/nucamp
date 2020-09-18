8aef8ae5a92d1ec5d99ef9576d261fdfbc33b9aa
jaymar85 <jermark851@gmail.com>
2020-09-17 23:48:53 -0500

    complete Mystery Theater

--- 1-HTML-CSS-JavaScript/workshop-3/variables.js
+++ 1-HTML-CSS-JavaScript/workshop-3/variables.js
@@ -1,13 +1,15 @@
-let name = 'Jen';
-name = 'Ben';
-console.log(name)
+// let name = 'Jen';
+// name = 'Ben';
+// console.log(name)
 
-const firstName = 'Emily';
-firstName = 'Emmy'
-console.log(firstName)
+// const firstName = 'Emily';
+// firstName = 'Emmy'
+// console.log(firstName)
 
 // var a = 1;
 // a = 2;
 
 // var a = 3
 // console.log(a)
+
+console.log(5 = '5')
