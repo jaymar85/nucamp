d117b8917485b1aa7e70740330215e07886ee453
Jeremy Mark <jermark851@gmail.com>
2021-12-12 23:22:41 -0600

    Add files via upload

---
+++ 2-Bootstrap/code-challenge-bootstrap-form/dist/script.js
@@ -0,0 +1,6 @@
+// You do not have to alter any of the code in this section for this challenge.
+
+function changeColor() {
+  const color = document.getElementById("colors").value;
+  document.body.style.backgroundColor = color;
+}
